574ecb286000bb0dc99234e5975ed46629c595fb
Moe <moe@studentscommission.ca>
2022-03-01 06:24:06 -0500

    CSS update

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,13 @@
-export default function ServiceCard({heading, desc, imgsrc}) {
+export default function ServiceCard({heading, desc, imgsrc, imgalt}) {
     return (
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div class="">
-                <div class="md:shrink-0">
-                <img class="h-48 w-full object-cover md:h-full md:w-full" src={imgsrc} alt="Man looking at item at a store"/>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+            <div className="">
+                <div className="md:shrink-0">
+                <img className="h-48 w-full object-cover md:h-full md:w-full" src={imgsrc} alt={imgalt}/>
                 </div>
-                <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{heading}</div>
-                <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Click to find out more about this service</a>
-                <p class="mt-2 text-slate-500 hidden md:inline-flex">{desc}</p>
+                <div className="p-8">
+                <div className="uppercase tracking-wide text-md md:text-sm text-indigo-500 font-semibold">{heading}</div>
+                <p className="mt-2 text-slate-500 hidden md:hidden lg:inline-flex">{desc}</p>
                 </div>
             </div>
         </div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,6 @@
 import Script from "next/script";
 import Image from "next/image";
+import Link from "next/link";
 export default function Navbar() {
     function toggle(){
         document.querySelector('#mobile-menu').classList.toggle('hidden');
@@ -11,10 +12,13 @@ export default function Navbar() {
     return (
         <nav className=" bg-white border-gray-200 px-2 w-full py-6 sticky top-0 z-50">
         <div className="container mx-auto flex flex-wrap items-center justify-between">
-            <a href="#" className="flex">
+            <Link href="/">
+            <a className="flex">
             <Image src="/images/logo.png" height={45} width={45} />
                 <span className="self-center text-lg font-semibold whitespace-nowrap">SummerSkin</span>
             </a>
+            </Link>
+            
             <button data-collapse-toggle="mobile-menu" type="button" className="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false" onClick={toggle}>
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
@@ -23,16 +27,24 @@ export default function Navbar() {
             <div className="hidden md:block w-full md:w-auto" id="mobile-menu">
             <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
                 <li>
-                <a href="/" className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">Home</a>
+                <Link href="/">
+                <a  className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">Home</a>
+                </Link>
                 </li>
                 <li>
-                <a href="/about" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">About</a>
+                <Link href="/about">
+                <a  className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">About</a>
+                </Link>
                 </li>
                 <li>
-                <a href="services" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Services</a>
+                <Link href="/services" >
+                <a  className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Services</a>
+                </Link>
                 </li>
                 <li>
-                <a href="contact" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Contact</a>
+                <Link href="/contact">
+                <a className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Contact</a>
+                </Link>
                 </li>
             </ul>
             </div>
