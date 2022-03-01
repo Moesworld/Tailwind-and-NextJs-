3a1f4496b039ff4015d9708e0826a06d4db75b61
Moe <moe@studentscommission.ca>
2022-03-01 07:11:23 -0500

    toggle navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,6 +4,8 @@ import Link from "next/link";
 export default function Navbar() {
     function toggle(){
         document.querySelector('#mobile-menu').classList.toggle('hidden');
+               
+        
 
     }
 
@@ -25,7 +27,7 @@ export default function Navbar() {
             <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
             </button>
             <div className="hidden md:block w-full md:w-auto" id="mobile-menu">
-            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium" onClick={toggle}>
                 <li>
                 <Link href="/">
                 <a  className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">Home</a>
